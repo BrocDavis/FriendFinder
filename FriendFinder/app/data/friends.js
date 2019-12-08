4e31867c57d1ae4efe9d475a53cd130f8d9a0816
Broc <broc.davis@yahoo.com>
2019-12-08 12:02:04 -0600

    added in pathing for routes in server. added in placeholder data for friend scores

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,34 @@
+let friendArray = [
+    {
+    name: "Friend1",
+    photo: "image to be added",
+    scores: [
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1
+    ]
+},
+{
+    name: "Friend2",
+    photo: "image to be added",
+    scores: [
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5
+    ]
+}
+]
