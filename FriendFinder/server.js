82bc44326b493a34fb792e436297f85cdb90785e
Broc <broc.davis@yahoo.com>
2019-12-08 23:03:06 -0600

    added correct linking to /api/friendslist for user to see friends in the database.

--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-let path = require("path");
 
 let app = express();
 let PORT = process.env.PORT || 3000;
@@ -9,14 +8,6 @@ app.use(express.json());
 
 require("./routing/apiRoutes")(app);
 require("./routing/htmlRoutes")(app);
-
-// app.get("/", function (req, res) {
-//     res.sendFile(path.join(__dirname, "home.html"));
-// });
-
-// app.get("/survey", function(req,res){
-//     res.sendFile(path.join(__dirname, "survey.html"))
-// });
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
